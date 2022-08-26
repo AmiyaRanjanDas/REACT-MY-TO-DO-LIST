ed658051f54b83db8137658490ead9bc84958752
Code with Amiya <amiyaranjandas2000@gmail.com>
2022-08-26 14:07:50 +0530

    update build for gh-pages

--- src/myComponent/header.js
+++ src/myComponent/header.js
@@ -8,8 +8,8 @@ export default function header({mode,fn}) {
       <>
         <nav className="navbar navbar-expand-lg " style={(mode==='Dark Mode')?({background:'rgb(0 64 255)'}):({background:'rgb(0 32 126)'})}>
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/" style={{color:'white'}}>TO-DO-LIST</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <Link className="navbar-brand" to="/" style={{color:'white'}} >TO-DO-LIST</Link>
+            <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
