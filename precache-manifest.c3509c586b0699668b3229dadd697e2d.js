99c65ab623ca33877549d68029cb6c5b40f53e49
Code with Amiya <amiyaranjandas2000@gmail.com>
2022-08-26 13:20:23 +0530

    Updates

---
+++ precache-manifest.c3509c586b0699668b3229dadd697e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f39dd5016390047022d",
+    "url": "/REACT-MY-TO-DO-LIST/static/css/main.f7564d33.chunk.css"
+  },
+  {
+    "revision": "7f39dd5016390047022d",
+    "url": "/REACT-MY-TO-DO-LIST/static/js/main.7f39dd50.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/REACT-MY-TO-DO-LIST/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "06b04ffdb65ec3c21312",
+    "url": "/REACT-MY-TO-DO-LIST/static/js/2.06b04ffd.chunk.js"
+  },
+  {
+    "revision": "88f26cd1e432dee67631",
+    "url": "/REACT-MY-TO-DO-LIST/static/js/runtime~main.88f26cd1.js"
+  },
+  {
+    "revision": "03051268d6d5d8edd78a08012382c484",
+    "url": "/REACT-MY-TO-DO-LIST/index.html"
+  }
+];
